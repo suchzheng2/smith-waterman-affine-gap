e4aaaca7ebf8c243956a32c4308d158aac4bfdd6
Suchen ZHENG <suzy897897@gmail.com>
2023-03-06 00:15:01 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-__author__ = "FirstName LastName"
-__email__ = "first.last@yale.edu"
-__copyright__ = "Copyright 2021"
+__author__ = "Suchen Zheng"
+__email__ = "suchen.zheng@yale.edu"
+__copyright__ = "Copyright 2023"
 __license__ = "GPL"
 __version__ = "1.0.0"
 
@@ -19,6 +19,7 @@ parser.add_argument('-i', '--input', help='input file', required=True)
 parser.add_argument('-s', '--score', help='score file', required=True)
 parser.add_argument('-o', '--opengap', help='open gap', required=False, default=-2)
 parser.add_argument('-e', '--extgap', help='extension gap', required=False, default=-1)
+parser.add_argument('-f', '--out_f_name', help='output file name', required=False, default="output.txt")
 args = parser.parse_args()
 
 ### Implement your Smith-Waterman Algorithm
@@ -33,57 +34,77 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 
 	#remove spaces in lines if there is any
 	input_lines = [line.strip() for line in input_lines]
-	print(input_lines)
+	print("input_lines: ",input_lines)
 	col_seq = input_lines[0]
 	row_seq = input_lines[1]
+	
 	# read score matrix
 	score_df = pd.read_fwf(scoreFile,sep='\t',index_col=0)
-	
-	score_mtx = np.zeros((len(row_seq)+1,len(col_seq)+1))
-	gap_mtx = np.zeros((len(row_seq)+1,len(col_seq)+1))
 
-	# filled_flag matrix is to record if the cell is filled or not 0-not filled, other-filled
-	# 1-filled with 0, 2-filled from diagnoal cell, 3-filled from top cell, 4-filled from left cell
-	filled_flag = np.zeros((len(row_seq)+1,len(col_seq)+1)) 
-
+	# place holder for maximun score and location of maximun score
 	global_max_score = 0
 	global_max_i = 0
 	global_max_j = 0
 
+	# initialize score matrix
+	# D - score from diagnal cells
+	# U - score from up cells
+	# L - score from left cells
+	# M - maxmimun score among all cases
 	D = np.zeros((len(row_seq)+1,len(col_seq)+1))
 	U = np.zeros((len(row_seq)+1,len(col_seq)+1))
 	L = np.zeros((len(row_seq)+1,len(col_seq)+1))
 	M = np.zeros((len(row_seq)+1,len(col_seq)+1))
 
-	# traceback matrix, 0-stop, 1-match, 2-up, 3-left
+	# traceback matrix, 0-stop, 1-diagnal, 2-up, 3-left
 	T = np.zeros((len(row_seq)+1,len(col_seq)+1)) 
 
 
-
-	for i in range(len(row_seq)):
+	# start alignment
+	for i,char_i in enumerate(row_seq):
 		i = i+1
-		for j in range(len(col_seq)):
-
-			
-			match_score = score_df[row_seq[i-1]][col_seq[j]]
+		for j,char_j in enumerate(col_seq):
 			j = j+1
+
+			# look up the match score from simnilarity matrix
+			match_score = score_df[char_i][char_j]
+
+			# possible score if it is a match - score from disganl cells 
 			D[i][j] = max(
-				D[i-1][j-1]+match_score,
+				M[i-1][j-1]+match_score,
 				U[i-1][j-1]+match_score,
-				L[i-1][j-1]+match_score
+				L[i-1][j-1]+match_score,
+				0
 				)
-
+			# possible score if it is a gap on the column sequence - score from up cells 
 			U[i][j] = max(
-				D[i-1][j]+openGap,
-				U[i-1][j]+extGap
-				)
-			L[i][j] = max(
-				D[i][j-1]+openGap,
-				L[i][j-1]+extGap
+				M[i-1][j]+openGap,
+				U[i-1][j]+extGap,
+				0
 				)
 
+			# possible score if it is a gap on the row sequence - score from left cells 
+			L[i][j] = max(
+				M[i][j-1]+openGap,
+				L[i][j-1]+extGap,
+				0
+				)
+
+			# maximum score among above cases
 			max_score = max(D[i][j],U[i][j],L[i][j],0)
 			M[i][j] = max_score
+
+			#--------------------------------------------------------
+			# Debug block: check intermediate step and look at all calculated values
+			# if i == 78 and j == 63:
+			# 	print(char_i,char_j)
+			# 	print(D[i][j],M[i-1][j-1]+match_score,U[i-1][j-1]+match_score,L[i-1][j-1]+match_score)
+			# 	print(U[i][j],M[i-1][j]+openGap, U[i-1][j]+extGap)
+			# 	print(L[i][j],M[i][j-1]+openGap,L[i][j-1]+extGap)
+			# 	print(max_score)
+
+			# 	print(L[0:4,32:34])
+			#--------------------------------------------------------
 
 			# update our max score recorder
 			if max_score>global_max_score:
@@ -102,15 +123,12 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 				T[i][j] = 3
 
 	## traceback
-
-	print(global_max_score)
 	## three result line holders
 	line1 = ""
 	line2 = ""
 	line3 = ""
 
 	# before start the traceback, filling the holder with unmatched end of sequence
-
 	row_seq_end = row_seq[global_max_i:]
 	col_seq_end = col_seq[global_max_j:]
 
@@ -121,20 +139,18 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 	line1 = ")"+row_seq_end
 	line2 = " "*len(line1)
 	line3 = ")"+col_seq_end
-
-	print(line1)
-	print(line2)
-	print(line3)
-
+	
 	## start traceback
 	current_i = global_max_i
 	current_j = global_max_j
 
+
+	# the traceback stop when one sequence end or a stop sign "0" met 
 	while (current_i > 0) and (current_j>0):
 		if T[current_i][current_j] == 0:
 			break
 
-		if T[current_i][current_j] == 1:
+		elif int(T[current_i][current_j]) == 1:
 			line1 = row_seq[current_i-1]+line1
 			line3 = col_seq[current_j-1]+line3
 			if row_seq[current_i-1] == col_seq[current_j-1]:
@@ -143,19 +159,18 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 				line2 = " "+line2
 			current_i = current_i -1
 			current_j = current_j -1
-		elif T[current_i][current_j] == 2:
+		elif int(T[current_i][current_j]) == 2:
 			line1 = row_seq[current_i-1]+line1
 			line2 = " "+line2
 			line3 = "-"+line3
 			current_i = current_i -1
-		elif T[current_i][current_j] == 3:
+		elif int(T[current_i][current_j]) == 3:
 			line1 = "-"+line1
 			line2 = " "+line2
 			line3 = col_seq[current_j-1]+line3
 			current_j = current_j -1
 
-		
-
+	# construct the prefix
 	row_seq_prefix = row_seq[0:current_i]
 	col_seq_prefix = col_seq[0:current_j]
 
@@ -166,42 +181,70 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 	line1 = row_seq_prefix+line1
 	line2 = " "*len(row_seq_prefix)+line2
 	line3 = col_seq_prefix+line3
+
+ 	# print out for immediate information
 	print(line3)
 	print(line2)
 	print(line1)
+	print("max score: ",global_max_score)
+
 
 	### write output
-	out_f = open("output.txt","w+")
+
+	# first section of output, sequnce information and score matrix title
+	out_f = open(args.out_f_name,"w+")
 	# out_f.writelines(["-----------\n", "|Sequences|\n", "-----------\n"])
 	for l in ["-----------\n", "|Sequences|\n", "-----------\n","sequence1\n",col_seq , "\nsequence2\n",row_seq,\
 	"\n--------------\n","|Score Matrix|\n","--------------\n"]:
 		out_f.write(l) 
 	out_f.close()
 
+	# save the score matrix to csv first and then 
 	M = M.astype(int)
-
-
 	score_df = pd.DataFrame(M,columns =[""]+[*col_seq])
-
 	# print(score_df.shape)
 	score_df.insert (0,"  ",[""]+[*row_seq])
-	print(score_df)
-	# dfAsString = score_df.to_string(index=False)
-	# out_f.write(dfAsString)
+	score_df.to_csv(args.out_f_name, sep ='\t',mode='a',index =False)
 
-	score_df.to_csv('output.txt', sep ='\t',mode='a',index =False)
 
-	out_f = open("output.txt","a")
+
+	#--------------------------------------------------------
+	# debug block - uncomment following block if need to save and look at each matrix
+	#--------------------------------------------------------
+	
+	# U_df = pd.DataFrame(U,columns =[""]+[*col_seq])
+	# U_df.insert (0,"  ",[""]+[*row_seq])
+	# U_df.to_csv("my_U_{}".format(args.input), sep ='\t',mode='a',index =False)
+
+	# M_df = pd.DataFrame(M,columns =[""]+[*col_seq])
+	# M_df.insert (0,"  ",[""]+[*row_seq])
+	# M_df.to_csv("my_M_{}".format(args.input), sep ='\t',mode='a',index =False)
+
+	# L_df = pd.DataFrame(L,columns =[""]+[*col_seq])
+	# L_df.insert (0,"  ",[""]+[*row_seq])
+	# L_df.to_csv("my_L_{}".format(args.input), sep ='\t',mode='a',index =False)
+
+	# T_df = pd.DataFrame(T,columns =[""]+[*col_seq])
+	# T_df.insert (0,"  ",[""]+[*row_seq])
+	# T_df.to_csv("my_T_{}".format(args.input), sep ='\t',mode='a',index =False)
+
+	#--------------------------------------------------------
+
+
+	
+	# save the final part of the result, the alignment
+	out_f = open(args.out_f_name,"a")
 	for l in ["----------------------\n", "|Best Local Alignment|\n", "----------------------\n",\
 	"Alignment Score:{}\n".format(int(global_max_score)),
 	"Alignment Results:\n",
 	line3,"\n",line2,"\n",line1,"\n"]:
-		out_f.write(l) 
+		out_f.write(l)
+
+	# message to user about the file saving information
+	print("results saved to {}".format(args.out_f_name))
 
 
 
-
-	
 
 ### Run your Smith-Waterman Algorithm
 runSW(args.input, args.score, args.opengap, args.extgap)
