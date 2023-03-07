e4b1e61c8f6ce2c5636fa3ba8ab36463aa07ddf4
Suchen ZHENG <suzy897897@gmail.com>
2023-03-06 23:20:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,11 @@ setuptools.setup(
     name='smith-waterman-affine-gap',
     version='0.0.1',
     author='Suchen Zheng',
-    author_email='suchen.zheng@yale.edu',
+    author_email='suzy897897@gmail.com',
     description='function for smith-waterman local alignment with affine gap',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/suchzheng2/smith-waterman-affine-gap',
-    project_urls = {
-        "Bug Tracker": "https://github.com/suchzheng2/smith-waterman-affine-gap/issues"
-    },
-    license='Yale',
+    license='MIT',
     packages=['smith-waterman-affine-gap'],
-    install_requires=['requests'],
 )
